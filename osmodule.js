69aea214a6c1812dff863b7b83e8553b0234921b
ismail61 <ismailhosen1061@gmail.com>
2021-01-28 17:34:02 +0600

    new file added called -> osmodule.js

---
+++ osmodule.js
@@ -0,0 +1,7 @@
+var os = require('os');
+console.log(os.hostname())
+console.log(os.networkInterfaces())
+console.log(os.uptime())
+console.log(os.version())
+console.log(os.cpus())
+console.log(os.totalmem())
